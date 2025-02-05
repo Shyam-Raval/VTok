21f1b64761b3fc8b68477702e042743a7f4519e3
Shyam-Raval <109334692+Shyam-Raval@users.noreply.github.com>
2025-02-05 21:40:16 +0530

    3rd commit

---
+++ app/src/main/java/com/example/vtok/NavigationRoutes.kt
@@ -0,0 +1,12 @@
+package com.example.vtok
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object StartScreen
+
+@Serializable
+object SignInScreen
+
+@Serializable
+object ChatsScreen
